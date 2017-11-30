4d70707edf93af94e04f178e4f7c6cd5a740f902
Chuchi <fabricagd>
2017-11-30 04:59:05 -0400

    Madrugada Jueves

--- app/src/main/java/com/obispo/fletare/PrincipiaActivity.java
+++ app/src/main/java/com/obispo/fletare/PrincipiaActivity.java
@@ -1,91 +1,92 @@
 package com.obispo.fletare;
 
-import android.database.Cursor;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-import android.widget.Spinner;
 import android.widget.TextView;
 
-public class PrincipiaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class PrincipiaActivity extends AppCompatActivity implements View.OnClickListener {
     MiAyudanteSQLite Carlo;
 
-    Spinner SPN11, SPN12;
     TextView TXV11, TXV12, TXV13;
-    Cursor Juan;
-    int paso =0;
-    long pedro=89;
+    ImageButton IMBTN10;
+    int punteos=0;  // Bandera de paso por los dialogs de ORIGEN
+    long CiudadOrigen=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principia);
 
-        TXV11=(TextView)findViewById(R.id.TXV11);
-        TXV12=(TextView)findViewById(R.id.TXV12);
-        TXV13=(TextView)findViewById(R.id.TXV13);
-        SPN11=(Spinner)findViewById(R.id.SPN11) ;
-        SPN12=(Spinner)findViewById(R.id.SPN12) ;
+        TXV11 = (TextView) findViewById(R.id.TXV11);
+        TXV12 = (TextView) findViewById(R.id.TXV12);
+        TXV13 = (TextView) findViewById(R.id.TXV13);
+         IMBTN10 = (ImageButton) findViewById(R.id.IMBTN10);
 
-        SPN11.setOnItemSelectedListener(this);
+
+
+        IMBTN10.setOnClickListener(this);
         Carlo = new MiAyudanteSQLite(this);
         Carlo.AbrirBase();
-        //Juan=Carlo.PueblaDepartamentos();
-        Cursor calido = Carlo.PueblaDepartamentos();
-
-
-
-        SimpleCursorAdapter acoplame = new SimpleCursorAdapter(this, R.layout.venturi,  calido, new String[] {"_id","Departamento"}, new int[] {R.id.venturi1,R.id.venturi2} );
-
-        //Carlo.AbrirBase();
-        //Carlo.metida();
-       //Carlo.metida();
-
-
-     // pedro = Carlo.CuantosHay();
-    //     acoplame.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        SPN11.setAdapter(acoplame);
-        SPN11.setSelection(9);
-       // String carrusel= String.valueOf(pedro);
-        Carlo.CerrarBase();
 
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long id) {
+    public void onClick(View view) {
 
-        switch (adapterView.getId()){
-            case R.id.SPN11:
-                if (paso ==0 &&  i!=9){
+        switch (view.getId()){
+
+            case R.id.IMBTN10: // ImageButton ORIGEN
+
+                    final Dialog dialog = new Dialog(this);
+                    punteos =0;
+                    CiudadOrigen = 0;
+                    dialog.setContentView(R.layout.forma_dialogo);
+                    final ListView LV10 = (ListView) dialog.findViewById(R.id.LV10);
+                    final TextView TXV99 = (TextView) dialog.findViewById(R.id.TXV99);
                     Carlo.AbrirBase();
-                    String gustavo = String.valueOf(id);
-                    Cursor frio = Carlo.PueblaCiudadesDeDepartamento(gustavo);
-                    SimpleCursorAdapter acoplamos = new SimpleCursorAdapter(this, R.layout.venturi, frio, new String[]{"_id", "Ciudad"}, new int[]{R.id.venturi1, R.id.venturi2});
-                    SPN11.setAdapter(acoplamos);
-                    SPN11.setSelection(0);
-                    Carlo.CerrarBase();
-                    paso++;
-                }
-                if (paso >0){
-                    Carlo.AbrirBase();
-                    String gustavin = String.valueOf(id);
-                    Cursor carrete = Carlo.BuscaCiudad(gustavin);
-                    carrete.moveToFirst();
+                    SimpleCursorAdapter paco = new  SimpleCursorAdapter(this, R.layout.venturi_doble, Carlo.PueblaDepartamentos(), new String[]{"_id", "Departamento"}, new int[]{0,R.id.venturi22});
 
-                    TXV13.setText(carrete.getString(2));
-                    TXV13.setVisibility(View.VISIBLE);
-                    Carlo.CerrarBase();
 
-                }
-            break;
-        }
+                    //Carlo.CerrarBase();
+                 LV10.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
+                            // Direcciona en el primer punteo
+
+                            if (punteos >0) {
+                                String casino = String.valueOf(id);
+                                CiudadOrigen = id;
+                                //punteos=0;
+                                TXV13.setText(casino);
+                                TXV13.setVisibility(View.VISIBLE);
+                               // Carlo.CerrarBase();
+                                dialog.cancel();
+                            }
+                            if (punteos ==0) {
+                                String paulov = String.valueOf(id);
+                                Carlo.AbrirBase();
+                                SimpleCursorAdapter pepe = new SimpleCursorAdapter(view.getContext(), R.layout.venturi_doble, Carlo.PueblaCiudadesDeDepartamento(paulov),new String[]{"_id", "Ciudad"}, new int[]{0, R.id.venturi22});
+                                LV10.setAdapter(pepe);
+                                TXV99.setText("Elija una Ciudad");
+                                punteos ++;
+                                Carlo.CerrarBase();
+                            }
+                        }
+                    });
+
+                    LV10.setAdapter(paco);
+                   // dialog.setTitle("Elija el Departamento");
+                    dialog.show();
+                    Carlo.CerrarBase();
+                    break;
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
 
-
-    }
 }
+    }
